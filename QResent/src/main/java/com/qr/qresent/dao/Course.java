4ad26e940a381ce25db7fc9fc0945f68783a85d0
Marinela-Denisa ȘTEFAN (95809) <marinela.stefan1708@stud.etti.upb.ro>
2021-11-23 20:56:34 +0200

    added new column for Course table

--- QResent/src/main/java/com/qr/qresent/dao/Course.java
+++ QResent/src/main/java/com/qr/qresent/dao/Course.java
@@ -19,13 +19,17 @@ public class Course {
     private Integer teacherId;
     @Column(name = "date")
     private String date;
+    @Column(name = "qrToken")
+    private String qrToken;
 
-    public Course(Integer ID, String courseName, Integer studentId, Integer teacherId, String date) {
+
+    public Course(Integer ID, String courseName, Integer studentId, Integer teacherId, String date, String qrToken) {
         this.ID = ID;
         this.courseName = courseName;
         this.studentId = studentId;
         this.teacherId = teacherId;
         this.date = date;
+        this.qrToken = qrToken;
     }
 
     public Course(String courseName, Integer studentId, Integer teacherId, String date) {
